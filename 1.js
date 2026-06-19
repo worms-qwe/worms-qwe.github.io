e71209a74605213a1d2a1522d4b788f5a24d3252
worms-qwe <78435451+worms-qwe@users.noreply.github.com>
2026-06-20 01:03:42 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -618,8 +618,6 @@
     parts.push('SegmentContainer=ts');
     parts.push('MinSegments=1');
     parts.push('BreakOnNonKeyFrames=false');
-    parts.push('h264-rangetype=SDR');
-    //parts.push('alwaysBurnInSubtitleWhenTranscoding=true');
 
     appendTranscodeQualityParams(parts, opts.qualityPreset);
     return apiBase() + '/Videos/' + encodeURIComponent(id) + '/master.m3u8?' + parts.join('&');
