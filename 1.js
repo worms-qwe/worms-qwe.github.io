bbc42019db82171237a5ffc2070233fd59adacf7
worms-qwe <78435451+worms-qwe@users.noreply.github.com>
2026-06-21 23:29:13 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -530,21 +530,11 @@
         "MaxStaticBitrate": 100000000,
         "MusicStreamingTranscodingBitrate": 384000,
         "DirectPlayProfiles": [
-          { "Container": "webm", "Type": "Video", "VideoCodec": "vp8,vp9,av1", "AudioCodec": "vorbis,opus" },
-          { "Container": "mp4,m4v", "Type": "Video", "VideoCodec": "h264,vp9,av1", "AudioCodec": "aac,mp3,mp2,opus,flac,vorbis" },
-          { "Container": "mkv", "Type": "Video", "VideoCodec": "h264,vp9,av1", "AudioCodec": "aac,mp3,mp2,opus,flac,vorbis" },
-          { "Container": "mov", "Type": "Video", "VideoCodec": "h264", "AudioCodec": "aac,mp3,mp2,opus,flac,vorbis" },
-          { "Container": "opus", "Type": "Audio" },
-          { "Container": "webm", "AudioCodec": "opus", "Type": "Audio" },
+          { "Container": "webm", "Type": "Video", "VideoCodec": "av1", "AudioCodec": "vorbis,opus" },
+          { "Container": "mp4,m4v", "Type": "Video", "VideoCodec": "h264,av1", "AudioCodec": "aac,mp3,mp2,opus,flac,vorbis" },
+          //{ "Container": "mkv", "Type": "Video", "VideoCodec": "h264,av1", "AudioCodec": "aac,mp3,mp2,opus,flac,vorbis" },
           { "Container": "ts", "AudioCodec": "mp3", "Type": "Audio" },
           { "Container": "mp3", "Type": "Audio" },
-          { "Container": "aac", "Type": "Audio" },
-          { "Container": "m4a", "AudioCodec": "aac", "Type": "Audio" },
-          { "Container": "m4b", "AudioCodec": "aac", "Type": "Audio" },
-          { "Container": "flac", "Type": "Audio" },
-          { "Container": "webma", "Type": "Audio" },
-          { "Container": "webm", "AudioCodec": "webma", "Type": "Audio" },
-          { "Container": "wav", "Type": "Audio" },
           { "Container": "ogg", "Type": "Audio" },
           { "Container": "hls", "Type": "Video", "VideoCodec": "av1,h264,vp9", "AudioCodec": "aac,mp2,opus,flac" },
           { "Container": "hls", "Type": "Video", "VideoCodec": "h264", "AudioCodec": "aac,mp3,mp2" }
@@ -557,19 +547,17 @@
         "ContainerProfiles": [],
         "CodecProfiles": [
           { "Type": "VideoAudio", "Codec": "aac", "Conditions": [ { "Condition": "Equals", "Property": "IsSecondaryAudio", "Value": "false", "IsRequired": false } ] },
-          { "Type": "VideoAudio", "Conditions": [ { "Condition": "Equals", "Property": "IsSecondaryAudio", "Value": "false", "IsRequired": false } ] },
-          { "Type": "Video", "Codec": "h264", "Conditions": [ { "Condition": "NotEquals", "Property": "IsAnamorphic", "Value": "true", "IsRequired": false }, { "Condition": "EqualsAny", "Property": "VideoProfile", "Value": "high|main|baseline|constrained baseline|high 10", "IsRequired": false }, { "Condition": "EqualsAny", "Property": "VideoRangeType", "Value": "SDR", "IsRequired": false }, { "Condition": "LessThanEqual", "Property": "VideoLevel", "Value": "52", "IsRequired": false }, { "Condition": "NotEquals", "Property": "IsInterlaced", "Value": "true", "IsRequired": false } ] },
-          { "Type": "Video", "Codec": "av1", "Conditions": [ { "Condition": "NotEquals", "Property": "IsAnamorphic", "Value": "true", "IsRequired": false }, { "Condition": "EqualsAny", "Property": "VideoProfile", "Value": "main", "IsRequired": false }, { "Condition": "EqualsAny", "Property": "VideoRangeType", "Value": "SDR|HDR10|HDR10Plus|HLG", "IsRequired": false }, { "Condition": "LessThanEqual", "Property": "VideoLevel", "Value": "19", "IsRequired": false } ] }
+          { "Type": "Video", "Codec": "h264", "Conditions": [ { "Condition": "NotEquals", "Property": "IsAnamorphic", "Value": "true", "IsRequired": false }, { "Condition": "EqualsAny", "Property": "VideoProfile", "Value": "high|main|baseline|constrained baseline", "IsRequired": false }, { "Condition": "EqualsAny", "Property": "VideoRangeType", "Value": "SDR", "IsRequired": false }, { "Condition": "LessThanEqual", "Property": "VideoLevel", "Value": "52", "IsRequired": false }, { "Condition": "NotEquals", "Property": "IsInterlaced", "Value": "true", "IsRequired": false } ] }
         ],
         "SubtitleProfiles": [
           { "Format": "subrip", "Method": "External" },
           { "Format": "ass", "Method": "External" },
           { "Format": "ssa", "Method": "External" }
         //],
-        [
+        ]
         //"ResponseProfiles": [
         //  { "Type": "Video", "Container": "m4v", "MimeType": "video/mp4" }
-        ]
+        //]
       }
     };
   
